18908ccdf6413eeed3399603727abe883aef412f
RexshackGaming <rexshackuk@gmail.com>
2024-05-14 06:29:18 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {
+  defaultRadius = 50.0,
+
+  colors = {
+    -- note: dont edit key/index, aligns with html
+    ['--eye-color-active']  = 'rgba(255,26,26, 0.7)',
+    ['--eye-color']         = 'rgba(255,255,255, 0.8)',
+    ['--title-color']       = 'rgba(255,51,51, 0.7)',
+    ['--text-color']        = 'rgba(255,255,255, 0.8)',
+  }
+}
